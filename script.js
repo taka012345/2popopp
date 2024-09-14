401f6abe4edd5dad9e3e1eb6e68134793f6456a0
taka012345 <117965802+taka012345@users.noreply.github.com>
2024-09-14 22:45:46 +0900

    Add files via upload

--- script.js
+++ script.js
@@ -6,14 +6,19 @@ window.addEventListener('scroll', function() {
     let scrollTop = window.pageYOffset || document.documentElement.scrollTop;
 
     // スクロールの変化量が閾値を超える場合のみ処理
-    if (Math.abs(scrollTop - lastScrollTop) > threshold) {
-        if (scrollTop > lastScrollTop) {
-            // 下にスクロールしているときはヘッダーを隠す
-            header.style.top = '-80px';
-        } else {
-            // 上にスクロールしているときはヘッダーを表示
-            header.style.top = '0';
+    if (scrollTop === 0) {
+        header.style.top = '0';
+    } else {
+        if (Math.abs(scrollTop - lastScrollTop) > threshold) {
+            if (scrollTop > lastScrollTop) {
+                // 下にスクロールしているときはヘッダーを隠す
+                header.style.top = '-80px';
+            } else {
+                // 上にスクロールしているときはヘッダーを表示
+                header.style.top = '0';
+            }
+            lastScrollTop = scrollTop; // 現在のスクロール位置を更新
         }
-        lastScrollTop = scrollTop; // 現在のスクロール位置を更新
+    
     }
 });
